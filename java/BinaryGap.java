0b70d2bec77c02f42e9eb135905f6ceea5ca7646
Alex Nevsky <anwer.man@gmail.com>
2017-01-19 00:36:10 +0300

    BinaryGap impl added

---
+++ java/BinaryGap.java
@@ -0,0 +1,33 @@
+package com.alexnevsky.algorithms;
+
+/**
+ * Created by Alex Nevsky on 1/19/17.
+ *
+ * Iterations
+ * Find longest sequence of zeros in binary representation of an integer.
+ */
+public class BinaryGap {
+
+    // Iterations
+    //
+    // Find longest sequence of zeros
+    // in binary representation of an integer.
+    public static int binaryGap(int N) {
+        int max = 0;
+
+        String binaryString = Integer.toBinaryString(N);
+        int localMax = 0;
+        for (int i = 0; i < binaryString.length(); ++i) {
+            if (binaryString.codePointAt(i) == 48) {
+                ++localMax;
+            } else {
+                if (max < localMax) {
+                    max = localMax;
+                }
+                localMax = 0;
+            }
+        }
+
+        return max;
+    }
+}
