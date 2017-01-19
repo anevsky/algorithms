51da3b5ac0e4e0b4303fe86fb9e353b0e58909b6
Alex Nevsky <anwer.man@gmail.com>
2017-01-19 21:08:15 +0300

    refactoring

--- go/main.go
+++ go/main.go
@@ -11,5 +11,5 @@ func main() {
     a := []int{9, 4, 2, 8, 3, 5, 7, 10, 1, 6}
     fmt.Println(a)
 
-    search.SentinelLinearSearch(a, 10, 1)
+    search.SentinelLinearSearch(a, 10, 6)
 }
--- go/search/sentinel-linear-search.go
+++ go/search/sentinel-linear-search.go
@@ -3,25 +3,27 @@ package search
 import "fmt"
 
 func SentinelLinearSearch(A []int, n int, x int) int {
-    fmt.Printf("hello, search\n")
-    fmt.Println("input: ", A, n, x)
+    fmt.Printf("Hello, SentinelLinearSearch!\n")
+    fmt.Println("Input: ", A, n, x)
 
-    last := A[n - 1]
-    A[n - 1] = x
-    i := 1
-    for ; A[i - 1] != x; i++ {
-      //
+    zn := n - 1
+
+    last := A[zn]
+    A[zn] = x
+    i := 0
+    for ; A[i] != x; i++ {
+      // ok
     }
-    A[n - 1] = last
+    A[zn] = last
 
     var result int
-    if i < n || A[n - 1] == x {
-        result = i - 1
+    if i < zn || A[zn] == x {
+        result = i
     } else {
       result = -1
     }
 
-    fmt.Printf("element %v has index %v\n", x, result)
+    fmt.Printf("Result: element %v has index %v\n", x, result)
 
     return result
 }
