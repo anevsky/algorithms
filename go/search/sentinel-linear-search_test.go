f5e88a04abbfc283e2f7005aa565ebf9eb597334
Alex Nevsky <anwer.man@gmail.com>
2017-01-19 21:47:10 +0300

    refactoring

--- go/search/sentinel-linear-search_test.go
+++ go/search/sentinel-linear-search_test.go
@@ -3,20 +3,22 @@ package search
 import "testing"
 
 func TestSentinelLinearSearch(t *testing.T) {
+    t.Log("Test last, first and middle elements (first element has index 0).")
+
     a := []int{9, 4, 2, 8, 3, 5, 7, 10, 1, 6}
 
-    r1 := SentinelLinearSearch(a, 10, 6)
-    if r1 != 9 {
-      t.Error("Expected 9, got ", r1)
+    i1 := SentinelLinearSearch(a, 10, 6)
+    if i1 != 9 {
+      t.Error("Expected 9, got", i1)
     }
 
-    r2 := SentinelLinearSearch(a, 10, 9)
-    if r2 != 0 {
-      t.Error("Expected 0, got ", r2)
+    i2 := SentinelLinearSearch(a, 10, 9)
+    if i2 != 0 {
+      t.Error("Expected 0, got", i2)
     }
 
-    r3 := SentinelLinearSearch(a, 10, 8)
-    if r3 != 3 {
-      t.Error("Expected 0, got ", r3)
+    i3 := SentinelLinearSearch(a, 10, 8)
+    if i3 != 3 {
+      t.Error("Expected 0, got", i3)
     }
 }
