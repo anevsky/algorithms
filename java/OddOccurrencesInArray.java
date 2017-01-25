2d67d6f98a356a033234d329f7ea7cd46c88b94e
Alex Nevsky <anwer.man@gmail.com>
2017-01-25 22:34:44 +0300

    OddOccurrencesInArray notes

--- java/OddOccurrencesInArray.java
+++ java/OddOccurrencesInArray.java
@@ -5,6 +5,13 @@ package com.alexnevsky.algorithms;
  *
  * Arrays
  * Find value that occurs in odd number of elements.
+ *
+ * XOR of all elements gives us odd occurring element.
+ * XOR of two elements is 0 if both elements are same.
+ * XOR of a number x with 0 is x.
+ *
+ * Y ^ 0 = Y
+ * X ^ X ^ Y = Y
  */
 public class OddOccurrencesInArray {
 
